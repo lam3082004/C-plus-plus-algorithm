9a89af2547f5fe059cf558f63bc4bd4ccf0bdaf6
lam3082004 <lam3082004@gmail.com>
2021-08-14 19:56:17 +0700

    14/08/2021

---
+++ Duyên Hải 2021 - Coastal/BITSTR/BITSTR.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define fo(i, a, b) for (int i = a; i <= b; i++)
+#define nmax 1005
+using namespace std;
+int main()
+{
+//#ifndef ONLINE_JUDGE
+//    freopen("l.inp", "r", stdin);
+//    freopen("l.out", "w", stdout);
+//#endif // ONLINE_JUDGE
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    cout << 1;
+}
---
+++ Duyên Hải 2021 - Coastal/KIOSKS/KIOSKS.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define fo(i, a, b) for (int i = a; i <= b; i++)
+#define nmax 100005
+using namespace std;
+int n,k,a[nmax],x,y;
+int main()
+{
+//#ifndef ONLINE_JUDGE
+//    freopen("l.inp", "r", stdin);
+//    freopen("l.out", "w", stdout);
+//#endif // ONLINE_JUDGE
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    cin>>n>>k;
+    fo(i,1,n) cin>>a[i];
+    fo(i,1,n-1)
+    {
+        cin>>x>>y;
+        if(x==1) dem++;
+    }
+    if(k!=1)
+    {
+        cout<<1;
+        break;
+    }
+    cout<<dem;
+}
