118e6cbcd21f01c98ed1b32f7f954e1e176d0d13
lam3082004 <lam3082004@gmail.com>
2021-11-05 12:28:07 +0700

    05/11/2021

---
+++ Duyên Hải 2021 - Coastal/a.html/l.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define fo(i, a, b) for (int i = a; i <= b; ++i)
+#define nmax 1000005
+#define ii pair<int, int>
+#define fi first
+#define se second
+using namespace std;
+const int mod = 1e9 + 7;
+int a[4], b, c, d, t;
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    cout<<1;
+}
